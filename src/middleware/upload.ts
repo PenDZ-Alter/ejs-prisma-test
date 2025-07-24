6392d1e735dd34ba4351da744da737b477402373
PenDZ-Alter <abxpendz45@gmail.com>
2025-07-24 15:05:55 +0700

    feat: adding file filter

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,6 +1,8 @@
 import multer from 'multer';
 import path from 'path';
 
+const allowedExtensions = ['.pdf', '.docx', '.doc', '.jpg', '.jpeg', '.png'];
+
 // Konfigurasi penyimpanan file
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
@@ -13,4 +15,14 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+  const ext = path.extname(file.originalname).toLowerCase();
+  
+  if (allowedExtensions.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error(`Only these file types are allowed: ${allowedExtensions.join(', ')}`));
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
